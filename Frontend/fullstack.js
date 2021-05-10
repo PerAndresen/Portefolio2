2ac9da2dfd54a1183983aa31ea449e71e2e3b980
Per Andresen <per2705@gmail.com>
2021-05-10 16:13:14 +0200

    startet smått på funksjoner

---
+++ Frontend/fullstack.js
@@ -0,0 +1,27 @@
+function addToCart(){
+    /* Knappfunksjon ved hvert produkt for å legge det til i handlelisten
+    Tar inn det som er tilhørende objekt
+    */
+   var name= document.getElementsByClassName('product')
+   
+    
+}
+
+
+
+function SaveItem(){
+
+
+}
+
+function ChangeItem(){
+
+}
+
+function RemoveItem(){
+
+}
+
+function ClearAll(){
+
+}
