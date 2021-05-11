8ca4ddcf42932806a569fec0faacb001027ea2d2
Per Andresen <per2705@gmail.com>
2021-05-11 17:06:07 +0200

    Importert JQUERY startet på metoder i JS

--- Frontend/fullstack.js
+++ Frontend/fullstack.js
@@ -1,10 +1,26 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+/*IMPORTERER JQUERY*/
+
 function addToCart(){
-    /* Knappfunksjon ved hvert produkt for å legge det til i handlelisten
+    /* 
+    Knappfunksjon ved hvert produkt for å legge det til i handlelisten
     Tar inn det som er tilhørende objekt
     */
-   var productname = document.getElementsByID('product')
-   var price = document.getElementsByClassName('price')
-        
+   let  productname = $("#productname").html() 
+   let price = $("#price").html()
+   /*let quantity = document.getElementById('') */
+   const product ={
+       productname: productname,
+       quantity: null,
+       price: price,
+   }
+   let sum= product.price * product.quantity
+   let ut = + "<tr><td>"+ product.productname +"</td><td>" + product.quantity + "</td><td>" + sum + "</td>"
+   return $("#listOfItems").html(ut)     
 }
 
 
