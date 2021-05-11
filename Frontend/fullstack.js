b0cd48ca88af49e2256cb754fcb5b03f7a285ca5
Per Andresen <per2705@gmail.com>
2021-05-11 13:35:16 +0200

    Start dag 2

--- Frontend/fullstack.js
+++ Frontend/fullstack.js
@@ -2,9 +2,9 @@ function addToCart(){
     /* Knappfunksjon ved hvert produkt for å legge det til i handlelisten
     Tar inn det som er tilhørende objekt
     */
-   var name= document.getElementsByClassName('product')
-   
-    
+   var productname = document.getElementsByID('product')
+   var price = document.getElementsByClassName('price')
+        
 }
 
 
