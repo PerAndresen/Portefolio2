b4156fb5db1c9c0c0650590c2d5be80002ff938a
Per Andresen <per2705@gmail.com>
2021-05-12 15:09:02 +0200

    fått adtocart metoden til å funke

--- Frontend/fullstack.js
+++ Frontend/fullstack.js
@@ -1,11 +1,29 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded',ready)
+} else {
+    ready()
+}
+
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 /*IMPORTERER JQUERY*/
+function ready(){
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for(var i = 0; i < addToCartButtons.length; i++){
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
 
-function addToCart(event){
+    var clearItemsbutton = document.getElementsByClassName('clear-items-button')
+    clearItemsbutton.addEventListener('click', clearCart)
+
+}
+
+
+function addToCart(){
     /* 
     Knappfunksjon ved hvert produkt for å legge det til i handlelisten
     Tar inn det som er tilhørende objekt
@@ -24,8 +42,6 @@ function addToCart(event){
    let price = $("#price").html()
    let quantity = $("#quantity1").val()
    /*let quantity = document.getElementById('') */
-   var buttonClicked = event.target
-   console.log(buttonClicked.parentElement.child(".productname"))
    const product ={
        productname: productname.text(),
        quantity: quantity.text(),
@@ -38,15 +54,47 @@ function addToCart(event){
    return $("#listOfItems").append(ut)     
 }
 
+function addToCartClicked(event){
+    /* 
+    Knappfunksjon ved hvert produkt for å legge det til i handlelisten
+    Tar inn det som er tilhørende objekt
+    Lagre dette inn i APIet?
+    */
+    var button = event.target
+    var productin = button.parentElement /* finner diven til produktet som knappen finner seg i*/
+    var productname = productin.getElementsByClassName('productname')[0].innerText
+    var productprice = productin.getElementsByClassName('productprice')[0].innerText
+    var productquantity = productin.getElementsByClassName('quantity')[0].value
+    console.log(productname, productprice, productquantity)
+    var price = parseFloat(productprice)
+    var quantity = parseFloat(productquantity)
+    const product ={
+        productname: productname,
+        productprice: price,
+        productquantity: quantity
+    }
+    let sum= product.productprice * product.productquantity
+    let ut = "<tr><td>"+ product.productname +"</td><td>" + product.productquantity + "</td><td>" + sum + "</td>"
+    return $("#listOfItems").append(ut) 
+}
+
+
+
+
+
+
 function createProduct(){
     /*lage en funksjon som lager ett produkt.*/
 }
 
 
 function RemoveItem(){
-
+    /* ha en removeknapp ved hvert item som dukker opp i listen*/
 }
 
-function ClearAll(){
-
+function clearCart(event){
+    /* Sletter alt fra handlelisten
+    kaller på apiet?
+    Tømmer listen*/
+    return null
 }
