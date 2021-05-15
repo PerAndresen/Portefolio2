4ea79bba13fdc8a5660c44ffbb28d14f2c138095
gardaa <s339557@oslomet.no>
2021-05-15 19:13:20 +0200

    Laget modal

--- Frontend/fullstack.js
+++ Frontend/fullstack.js
@@ -53,8 +53,46 @@ function addToCartClicked(event){
     sumCart()
 }
 
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach( modal => {
+        closeModal(modal) 
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
 // Function to give more pictures/information about a product(get from database)
 function showMoreInformation() {
+
     console.log('About to fetch information about a product');
     fetch('http://127.0.0.1:5000/webshop/products/get/<product_id>').then(response => {
             console.log(response);
@@ -64,7 +102,6 @@ function showMoreInformation() {
             document.getElementById('')
         });
 }
-
 
 function addItemToCart(productname, productquantity, sum) {
     var cartLinje = document.createElement('div')
