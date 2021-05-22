1aa376e7a204408b0771769be935a92a0bbff024
Per Andresen <per2705@gmail.com>
2021-05-22 16:16:47 +0200

    opprettet docker file og dockercompose

--- Backend/server.py
+++ Backend/server.py
@@ -1,23 +1,63 @@
 # MySQL
 
+from flask import Flask, render_template, request
 import flask
 import mysql.connector
+from flask_mysqldb import MySQL
+
+app = flask.Flask(__name__, static_folder="/var/portefolio2/frontend",static_url_path="")
+app.config["DEBUG"] = True
+
+"""
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'webshop_database'
+mysql = MySQL(app)
+"""
 
 # Så enkelt fordi de er i samme docker nettverk, så vi burde få til det samme
 # Variables for everything needed to connect to the database
-user = 'admin'
-password= 'password'
-host = 'Her skal navn på databasen som du setter når den startes i docker(container)(35:30)'
-database = 'webshop_database'
+innuser = 'admin'
+innpassword= 'password'
+innhost = 's127.0.0.1'
+inndatabase = 'webshop_database'
 
 # Connect to the database
-db = mysql.connector.connect(user, password, host, database)
+db = mysql.connector.connect(user=innuser, password=innpassword, host=innhost, database=inndatabase)
 
 # Prints everything from the table 'products'
 cursor = db.cursor()
 cursor.execute('SELECT * FROM products')
 result = cursor.fetchall()
 
+@app.route('/form')
+def form():
+    return render_template('./index.html')
+
+# Product class
+class Product:
+    def __init__(self, name, price, description, image):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.image = image
+
+
+# Method for getting a product from the database 
+@app.route('/webshop/products/get/<product_id>', methods=['GET'])
+def getProduct(product_id):
+    # Finds and returns a document with specidic product_id
+    return None
+    
+
+# Method for adding a product to the database
+@app.route('/webshop/products/add/<name>/<price>/<description>/<image>', methods=['GET', 'POST'])
+def addProduct(name, price, description, image):
+   return None
+
+
+app.run(host='localhost',port=5000)
 
 # MONGODB
 """
